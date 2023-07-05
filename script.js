5b12006f2797e8dfba45e53b0a694bb7ccb371a4
Ja1roLopez <jairolopez21@outlook.es>
2023-07-05 06:55:01 -0500

    Ajuste comentarios

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-// Obtener los botones de la navbar
+
 const navbarButtons = document.querySelectorAll('.navbar a');
 
-// Función para resaltar el botón activo
+
 function resaltarBotonActivo() {
-  // Obtener la posición vertical actual de la ventana
+  
   const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
 
-  // Iterar sobre los botones de la navbar
+  
   navbarButtons.forEach((button) => {
     const targetSectionId = button.getAttribute('href').substring(1);
     const targetSection = document.getElementById(targetSectionId);
 
-    // Verificar si la sección está visible en la ventana
+    
     if (targetSection.offsetTop <= scrollPosition && targetSection.offsetTop + targetSection.offsetHeight > scrollPosition) {
-      // Remover la clase activa de todos los botones
+      
       navbarButtons.forEach((btn) => btn.classList.remove('active'));
 
-      // Agregar la clase activa al botón correspondiente
+      
       button.classList.add('active');
     }
   });
 }
 
-// Escuchar el evento scroll
+
 window.addEventListener('scroll', resaltarBotonActivo);
 
-// Smooth scroll al hacer clic en los botones de la navbar
+
 navbarButtons.forEach((button) => {
   button.addEventListener('click', (event) => {
     event.preventDefault(); // Evitar el comportamiento de desplazamiento predeterminado
@@ -33,10 +33,9 @@ navbarButtons.forEach((button) => {
     const targetSectionId = button.getAttribute('href').substring(1);
     const targetSection = document.getElementById(targetSectionId);
 
-    // Calcular la posición de desplazamiento objetivo
     const targetOffset = targetSection.offsetTop;
 
-    // Animar el desplazamiento suave
+    
     window.scrollTo({
       top: targetOffset,
       behavior: 'smooth'
@@ -44,12 +43,12 @@ navbarButtons.forEach((button) => {
   });
 });
 document.getElementById('circle-button').addEventListener('click', function(event) {
-    event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+    event.preventDefault(); 
   
-    // Obtiene la sección de perfil
+    
     var perfilSection = document.getElementById('perfil');
   
-    // Realiza el desplazamiento suave hacia la sección de perfil
+    
     perfilSection.scrollIntoView({
       behavior: 'smooth'
     });
@@ -60,7 +59,7 @@ function mostrarToast(){
   M.toast({html:'Enviado correctamente'});
 }
 function saveContact(event) {
-  event.preventDefault(); // Previene el comportamiento predeterminado de envío del formulario
+  event.preventDefault(); 
 
   let nameContact = document.getElementById("name");
   let emailContact = document.getElementById("email")
@@ -91,8 +90,8 @@ function saveContact(event) {
   });
 }
 
-// Obtén el formulario por su ID o cualquier otro selector
+
 const form = document.getElementById('my-form');
 
-// Agrega un controlador de eventos para el evento "submit" del formulario
+
 form.addEventListener('submit', saveContact);
